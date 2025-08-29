6001c6e3403de2f413645ced82308a2ced9f17f8
jmendes16 <joel.mendes@multiverse.io>
2025-08-29 09:20:32 +0100

    Revert "added simple function for dividing numbers"
    This reverts commit 1818a81bc3a0027713324eb7881890803f901892.

--- simple_function.py
+++
@@ -1,6 +0,0 @@
-# lets define some functions that might be used for data transformation
-def divide_numbers(a, b):
-    if b == 0:
-        raise ZeroDivisionError("The divisor cannot be zero")
-    result = a / b
-    return result
